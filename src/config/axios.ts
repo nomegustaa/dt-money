0996311a16e045a90deb48d8ea9f09e481b23a91
nomegustaa <gustavocarvalhodasilva015@gmail.com>
2024-07-28 13:47:34 -0300

    chore: changes for api vercel

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:4001/',
+  baseURL: 'https://api-vercel-git-develop-nomegustaas-projects.vercel.app/',
 })
